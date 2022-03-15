16684e582eb933204cbac203f6fb0b62524dfeb4
Aries Firmansyah <aries.firmansyah@student.umn.ac.id>
2022-03-15 16:22:46 +0700

    - update develop planning - Responsive Carousel - Responsive Summary Movie

--- src/components/MovieDetails/Carousel/index.js
+++ src/components/MovieDetails/Carousel/index.js
@@ -22,28 +22,27 @@ const Carousel = ({movie}) => {
             <Image src={`${poster_BaseURL}${movie.details_backdrop}`} 
                 className="movieDetailsCarousel-image" w="100%" />
             <div className='moviedetails-carousel1'>
-                <SimpleGrid columns={[7, 7, 7, 7, 7]} 
+                <SimpleGrid columns={[10, 10, 10, 7, 7]} 
                     spacing={{base: '10px', sm: '10px', md: '40px', lg: '40px', xl : '40px' }} 
                     className='moviedetails-carousel2'>
-                    <GridItem colSpan={{base : 2, sm: 2, md: 2, lg: 2, xl : 2 }} >
-                        <h1 style={{fontSize: '30px', marginTop: '3px'}}>
-                            {movie.details.original_title} <br></br>
-                            ({moment(movie.details.release_date).format('YYYY')})
+                    <GridItem colSpan={{base : 10, sm: 10, md: 10, lg: 2, xl : 2 }} >
+                        <h1 className='movie-details-title'>
+                            {movie.details.original_title} ({moment(movie.details.release_date).format('YYYY')})
                         </h1>
                     </GridItem>
-                    <GridItem colSpan={{base : 1, sm: 1, md: 1, lg: 1, xl : 1 }} >
+                    <GridItem colSpan={{base : 2, sm: 2, md: 2, lg: 1, xl : 1 }} >
                         <h1 style={{color: '#616161'}}>DURATION</h1>
                         <h2 style={{fontWeight: '500'}}>
                             {movie.details.runtime} m
                         </h2>
                     </GridItem>
-                    <GridItem colSpan={{base : 1, sm: 1, md: 1, lg: 1, xl : 1 }} >
+                    <GridItem colSpan={{base : 2, sm: 2, md: 2, lg: 1, xl : 1 }} >
                         <h1 style={{color: '#616161'}}>RATING</h1>
                         <h2 style={{fontWeight: '500'}}>
                             {movie.details.vote_average}
                         </h2>
                     </GridItem>
-                    <GridItem colSpan={{base : 2, sm: 2, md: 2, lg: 2, xl : 2 }} >
+                    <GridItem colSpan={{base : 3, sm: 3, md: 3, lg: 2, xl : 2 }} >
                         <h1 style={{color: '#616161'}}>
                             GENRE
                         </h1>
@@ -62,7 +61,7 @@ const Carousel = ({movie}) => {
                             }
                         </div>
                     </GridItem>
-                    <GridItem colSpan={{base : 1, sm: 1, md: 1, lg: 1, xl : 1 }} >
+                    <GridItem colSpan={{base : 3, sm: 3, md: 3, lg: 1, xl : 1 }} >
                         <h1 style={{color: '#616161'}}>
                             RELEASE DATE
                         </h1>
