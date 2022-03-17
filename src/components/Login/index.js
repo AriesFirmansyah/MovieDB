7fee771ee0b2a19db13f0e4849a79d8da65813fc
Aries Firmansyah <aries.firmansyah@student.umn.ac.id>
2022-03-17 11:44:28 +0700

    update responsive login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -10,7 +10,8 @@ import {
     InputGroup,
     Input,
     InputRightElement,
-    Divider
+    Divider,
+    Container
 } from '@chakra-ui/react';
 
 import {
@@ -111,124 +112,123 @@ const Login = () => {
                 
             </div>
             <div className='login-container'>
-                <SimpleGrid columns={[10, 10, 10, 10, 10]} 
-                    spacing={{base: '10px', sm: '10px', md: '40px', lg: '40px', xl : '40px' }}
-                    style={{textAlign: 'center'}}>
-                    <GridItem colSpan={{base : 10, sm: 10, md: 6, lg: 6, xl : 6 }} >
+                <Container maxW='container.xl'>
+                    <SimpleGrid columns={[10, 10, 10, 10, 10]} 
+                        spacing={{base: '10px', sm: '10px', md: '10px', lg: '0px', xl : '0px' }}
+                        style={{textAlign: 'center'}}>
+                        <GridItem colSpan={{base : 10, sm: 10, md: 6, lg: 6, xl : 6 }} >
 
-                        <h1 className='login-text-header'>Sign In to Get Your Movies</h1>
-                        <div className="login-option">
-                            <h1>
-                                Don't have an account ? <br></br>
-                                You can <span>Register here!</span>
-                            </h1>
-                            <img src={People} />
-                        </div>
-                    </GridItem>
-                    <GridItem colSpan={{base : 10, sm: 10, md: 4, lg: 4, xl : 4 }} >
-                        <Input placeholder='Enter email or phone number' 
-                            className='login-input' />
-                        <br></br><br></br>
-                        <InputGroup size='md'>
-                            <Input
-                                pr='4.5rem'
-                                type={showPassword ? 'text' : 'password'}
-                                placeholder='Password'
-                                style={{zIndex: 0}}
-                                className='login-input'
-                            />
-                            <InputRightElement width='4.5rem' sx={{zIndex: 1, marginRight: -3}}>
-                                { 
-                                    showPassword ? 
-                                        <div onClick={handleShowPassword}>
-                                            
-                                            <AiFillEye />
-                                        </div>
-                                        : 
-                                        <div onClick={handleShowPassword}>
-                                            <AiFillEyeInvisible />
-                                        </div>
-                                }
-                            </InputRightElement>
-                        </InputGroup>
-
-                        <Button colorScheme='blue' 
-                            style={{marginTop: '30px', marginBottom: '30px', width: '100%'}}>
-                            Sign In
-                        </Button>
-
-                        <div className="login-option-mobile">
-                            <h1>
-                                Don't have an account ? 
-                                You can <span>Register here!</span>
-                            </h1>
-                        </div>
-
-                        <SimpleGrid columns={10}>
-                            <GridItem colSpan={{base : 2, sm: 3, md: 2, lg: 3, xl : 3 }}>
-                                <Divider />
-                            </GridItem>
-                            <GridItem colSpan={{base : 6, sm: 4, md: 6, lg: 4, xl : 4 }}>
-                                <h1 style={{color: 'gray', marginTop: '-12px'}}>
-                                    or continue with
+                            <h1 className='login-text-header'>Sign In to Get Your Movies</h1>
+                            <div className="login-option">
+                                <h1>
+                                    Don't have an account ? <br></br>
+                                    You can <span>Register here!</span>
                                 </h1>
-                            </GridItem>
-                            <GridItem colSpan={{base : 2, sm: 3, md: 2, lg: 3, xl : 3 }}>
-                                <Divider />
-                            </GridItem>
-                        </SimpleGrid>
+                                <img src={People} />
+                            </div>
+                        </GridItem>
+                        <GridItem colSpan={{base : 10, sm: 10, md: 4, lg: 4, xl : 4 }} >
+                            <Input placeholder='Enter email or phone number' 
+                                className='login-input' />
+                            <br></br><br></br>
+                            <InputGroup size='md'>
+                                <Input
+                                    pr='4.5rem'
+                                    type={showPassword ? 'text' : 'password'}
+                                    placeholder='Password'
+                                    style={{zIndex: 0}}
+                                    className='login-input'
+                                />
+                                <InputRightElement width='4.5rem' sx={{zIndex: 1, marginRight: -3}}>
+                                    { 
+                                        showPassword ? 
+                                            <div onClick={handleShowPassword}>
+                                                
+                                                <AiFillEye />
+                                            </div>
+                                            : 
+                                            <div onClick={handleShowPassword}>
+                                                <AiFillEyeInvisible />
+                                            </div>
+                                    }
+                                </InputRightElement>
+                            </InputGroup>
 
-                        <SimpleGrid columns={[5, 5, 5, 10, 10]}  style={{marginTop: '20px'}}
-                            spacing={2}>
-                            <GridItem colSpan={5}>
-                                <GoogleLogin
-                                    clientId={clientID}
-                                    render={renderProps => (
-                                        <Button disabled={renderProps.disabled}
-                                            onClick={renderProps.onClick}
-                                            sx={{
-                                                width: '100%',
-                                                backgroundColor: 'aliceblue', 
-                                                // marginTop: '30px',
-                                                color: 'black'
-                                            }}>
-                                            <FcGoogle /> &nbsp;
-                                            Google
-                                        </Button>
-                                    )}
-                                    buttonText="Log in with Google"
-                                    onSuccess={handleGoogleLogin}
-                                    onFailure={handleGoogleFailure}
-                                    cookiePolicy={'single_host_origin'} />
-                            </GridItem>
-                            <GridItem colSpan={5}>
-                                <FacebookLogin
-                                    appId={facebookID}
-                                    render={renderProps => (
-                                        <Button disabled={renderProps.disabled}
-                                            onClick={renderProps.onClick}
-                                            sx={{
-                                                width: '100%',
-                                                backgroundColor: 'aliceblue', 
-                                                // marginTop: '10px',
-                                                color: 'black',
-                                                // marginLeft: '10px'
-                                            }}>
-                                            <BsFacebook style={{color: 'blue'}}/> &nbsp;
-                                            Facebook
-                                        </Button>
-                                    )}
-                                    // autoLoad={true}
-                                    fields="name,email,picture"
-                                    // onClick={componentClicked}
-                                    callback={handleFacebookLogin} />
-                            </GridItem>
-                        </SimpleGrid>
-                        
+                            <Button colorScheme='blue' 
+                                style={{marginTop: '30px', marginBottom: '30px', width: '100%'}}>
+                                Sign In
+                            </Button>
 
+                            <div className="login-option-mobile">
+                                <h1>
+                                    Don't have an account ? 
+                                    You can <span>Register here!</span>
+                                </h1>
+                            </div>
 
-                    </GridItem>
-                </SimpleGrid>
+                            <SimpleGrid columns={10}>
+                                <GridItem colSpan={{base : 2, sm: 3, md: 2, lg: 3, xl : 3 }}>
+                                    <Divider />
+                                </GridItem>
+                                <GridItem colSpan={{base : 6, sm: 4, md: 6, lg: 4, xl : 4 }}>
+                                    <h1 style={{color: 'gray', marginTop: '-12px'}}>
+                                        or continue with
+                                    </h1>
+                                </GridItem>
+                                <GridItem colSpan={{base : 2, sm: 3, md: 2, lg: 3, xl : 3 }}>
+                                    <Divider />
+                                </GridItem>
+                            </SimpleGrid>
+
+                            <SimpleGrid columns={[5, 5, 5, 10, 10]}  style={{marginTop: '20px'}}
+                                spacing={2}>
+                                <GridItem colSpan={5}>
+                                    <GoogleLogin
+                                        clientId={clientID}
+                                        render={renderProps => (
+                                            <Button disabled={renderProps.disabled}
+                                                onClick={renderProps.onClick}
+                                                sx={{
+                                                    width: '100%',
+                                                    backgroundColor: 'aliceblue', 
+                                                    // marginTop: '30px',
+                                                    color: 'black'
+                                                }}>
+                                                <FcGoogle /> &nbsp;
+                                                Google
+                                            </Button>
+                                        )}
+                                        buttonText="Log in with Google"
+                                        onSuccess={handleGoogleLogin}
+                                        onFailure={handleGoogleFailure}
+                                        cookiePolicy={'single_host_origin'} />
+                                </GridItem>
+                                <GridItem colSpan={5}>
+                                    <FacebookLogin
+                                        appId={facebookID}
+                                        render={renderProps => (
+                                            <Button disabled={renderProps.disabled}
+                                                onClick={renderProps.onClick}
+                                                sx={{
+                                                    width: '100%',
+                                                    backgroundColor: 'aliceblue', 
+                                                    // marginTop: '10px',
+                                                    color: 'black',
+                                                    // marginLeft: '10px'
+                                                }}>
+                                                <BsFacebook style={{color: 'blue'}}/> &nbsp;
+                                                Facebook
+                                            </Button>
+                                        )}
+                                        // autoLoad={true}
+                                        fields="name,email,picture"
+                                        // onClick={componentClicked}
+                                        callback={handleFacebookLogin} />
+                                </GridItem>
+                            </SimpleGrid>
+                        </GridItem>
+                    </SimpleGrid>
+                </Container>
             </div>
         </>
     );
